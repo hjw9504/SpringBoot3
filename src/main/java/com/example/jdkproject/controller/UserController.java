2cec8c4725e321a12a6f1122d6cae0da980acc29
hjw9504 <hjw9504@naver.com>
2025-02-02 22:00:55 +0900

    fix: kafka publish config topic update

--- src/main/java/com/example/jdkproject/controller/UserController.java
+++ src/main/java/com/example/jdkproject/controller/UserController.java
@@ -87,7 +87,7 @@ public class UserController {
         Member member = userService.login(loginDto.getUserId(), loginDto.getUserPw());
 
         //login message
-        kafkaProducer.sendMessage(member.getMemberId());
+        kafkaProducer.sendMessage(member);
 
         return new Response<>(member, HttpStatus.OK, SUCCESS);
     }
--- src/main/java/com/example/jdkproject/service/KafkaProducer.java
+++ src/main/java/com/example/jdkproject/service/KafkaProducer.java
@@ -1,12 +1,22 @@
 package com.example.jdkproject.service;
 
+import com.example.jdkproject.domain.Member;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Value;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@Slf4j
 @Service
 public class KafkaProducer {
-    private String topicName = "myTestTopic";
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+    private String topicName = "auth_login";
 
     private final KafkaTemplate<String, String> kafkaTemplate;
 
@@ -14,8 +24,21 @@ public class KafkaProducer {
         this.kafkaTemplate = kafkaTemplate;
     }
 
-    public void sendMessage(String message) {
-        System.out.println(String.format("Produce message : %s", message));
-        this.kafkaTemplate.send(topicName, message);
+    public void sendMessage(Member message) {
+        log.info("Produce message : {} {}", message.getUserId(), message.getName());
+        this.kafkaTemplate.send(topicName, getFilteredMap(message));
+    }
+
+    private String getFilteredMap(Member member) {
+        try {
+            Map<String, String> resultMap = new HashMap<>();
+            resultMap.put("memberId", member.getMemberId());
+            resultMap.put("name", member.getName());
+
+            return objectMapper.writeValueAsString(resultMap);
+        } catch(Exception e) {
+            log.warn("Exception: ", e);
+            return null;
+        }
     }
 }
