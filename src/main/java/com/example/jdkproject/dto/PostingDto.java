b55151adab45c3944a64b35a25bd02cf10126a64
hjw9504 <hjw9504@naver.com>
2026-04-04 22:40:05 +0900

    feat: PostingDto camel case에서 snake case로 업데이트

--- src/main/java/com/example/jdkproject/dto/PostingDto.java
+++ src/main/java/com/example/jdkproject/dto/PostingDto.java
@@ -1,10 +1,13 @@
 package com.example.jdkproject.dto;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class PostingDto {
     private int id;
     private String title;
